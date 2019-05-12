359a4e8793f9eccc9ca6fbd83e1a7f4d227aa214
therobertc <rob.jcalderon@gmail.com>
2019-05-12 14:34:44 -0700

    added styled components

--- src/App/index.js
+++ src/App/index.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import WelcomeMessage from './WelcomeMessage'
+import styled, {css} from 'styled-components';
 
-function App() {
-  return (
-    <div> Hello World
-    </div>
-  );
+
+class App extends Component{
+  render() {
+    return (
+      <div>
+        <WelcomeMessage/>
+      </div>
+      
+
+    );
+  }
 }
 
 export default App;
