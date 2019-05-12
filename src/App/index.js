a39b86b5efd858ae9ebf3ad2596602cf05d156ba
therobertc <rob.jcalderon@gmail.com>
2019-05-12 16:38:01 -0700

    add AppProvider and hook up to AppBar

--- src/App/index.js
+++ src/App/index.js
@@ -1,18 +1,21 @@
 import React, {Component} from 'react';
 import './App.css';
-import WelcomeMessage from './WelcomeMessage'
 import styled, {css} from 'styled-components';
 
+import WelcomeMessage from './WelcomeMessage'
 import AppLayout from './AppLayout';
 import AppBar from './AppBar';
+import {AppProvider} from './AppProvider';
 
 
 class App extends Component{
   render() {
     return (
       <AppLayout>
-        <AppBar/>
-        <WelcomeMessage/>
+        <AppProvider>
+          <AppBar/>
+          <WelcomeMessage/>
+        </AppProvider>
       </AppLayout>
       
 
