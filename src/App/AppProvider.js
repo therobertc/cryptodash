55585aee021854178172cc17750a3652ca653863
therobertc <rob.jcalderon@gmail.com>
2019-05-13 16:00:44 -0700

    added fetch prices

--- src/App/AppProvider.js
+++ src/App/AppProvider.js
@@ -25,11 +25,31 @@ export class AppProvider extends React.Component {
 
     componentDidMount = () => {
         this.fetchCoins();
+        this.fetchPrices();
     }
 
     fetchCoins = async () => {
         let coinList = (await cc.coinList()).Data;
         this.setState({coinList});
+    }
+
+    fetchPrices = async () => {
+        if(this.state.firstVisit) return;
+        let prices = await this.prices();
+        this.setState({prices});
+    }
+
+    prices = async () => {
+        let returnData = [];
+        for (let i = 0; i < this.state.favorites.length; i++) {
+            try {
+                let priceData = await cc.priceFull(this.state.favorites[i], 'USD');
+                returnData.push(priceData);
+            } catch (e){
+                console.warn('Fetch price error: ', e);
+            }
+        }
+        return returnData;
     }
 
     addCoin = key => {
@@ -52,6 +72,9 @@ export class AppProvider extends React.Component {
        this.setState({
            firstVisit: false,
            page: 'dashboard',
+
+       }, () => {
+           this.fetchPrices();
            
        });
        localStorage.setItem('cryptoDash', JSON.stringify({
