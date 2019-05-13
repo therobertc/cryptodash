373a6a94e966ea7b3c5190047d52ea580c2e78dd
therobertc <rob.jcalderon@gmail.com>
2019-05-13 00:30:19 -0700

    completed search

--- src/Settings/CoinGrid.js
+++ src/Settings/CoinGrid.js
@@ -11,16 +11,22 @@ grid-gap: 15px;
 margin-top: 40px;
 `
 
-function getCoinsToDisplay(coinList, topSection, favorites){
+function getLowerSectionCoins(coinList, filteredCoins){
+    return (filteredCoins && Object.keys(filteredCoins)) || 
+    Object.keys(coinList).slice(0, 100)
+
+}
+
+function getCoinsToDisplay(coinList, topSection, favorites, filterCoins){
     return (
-        topSection ? favorites : Object.keys(coinList).slice(0, 100)
+        topSection ? favorites : getLowerSectionCoins(coinList, filterCoins)
     )}
 
 export default function ({topSection}){
     return (
     <AppContext.Consumer>
-        {({coinList, favorites}) => <CoinGridStyled>
-            {getCoinsToDisplay(coinList, topSection, favorites).map(coinKey => 
+        {({coinList, favorites, filteredCoins}) => <CoinGridStyled>
+            {getCoinsToDisplay(coinList, topSection, favorites, filteredCoins).map(coinKey => 
             <CoinTile topSection={topSection} coinKey={coinKey}/> 
             )}
         </CoinGridStyled>}
