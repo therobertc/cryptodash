2a9b4d77e995549eecf7f24af52e4ba1ebdd293f
therobertc <rob.jcalderon@gmail.com>
2019-05-12 18:24:07 -0700

    added page

---
+++ src/Settings/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import WelcomeMessage from './WelcomeMessage';
+import ConfirmButton from './ConfirmButton';
+import Page from '../Shared/Page';
+
+export default function() {
+    return <Page name="settings"> <WelcomeMessage/> <ConfirmButton/> </Page>
+  }
