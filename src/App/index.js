5cf1cbd2fbf6a549b0d3a11d9ae7b3f5586f54fe
therobertc <rob.jcalderon@gmail.com>
2019-05-13 19:43:58 -0700

    dashboard grid

--- src/App/index.js
+++ src/App/index.js
@@ -6,6 +6,7 @@ import AppLayout from './AppLayout';
 import AppBar from './AppBar';
 import {AppProvider} from './AppProvider';
 import Settings from '../Settings';
+import Dashboard from '../Dashboard';
 import Content from '../Shared/Content'
 
 
@@ -17,6 +18,7 @@ class App extends Component{
           <AppBar/>
             <Content>
               <Settings/>
+              <Dashboard/>
             </Content>
         </AppProvider>
       </AppLayout>
--- src/Settings/CoinGrid.js
+++ src/Settings/CoinGrid.js
@@ -25,11 +25,13 @@ function getCoinsToDisplay(coinList, topSection, favorites, filterCoins){
 export default function ({topSection}){
     return (
     <AppContext.Consumer>
-        {({coinList, favorites, filteredCoins}) => <CoinGridStyled>
+        {({coinList, favorites, filteredCoins}) => (
+        <CoinGridStyled>
             {getCoinsToDisplay(coinList, topSection, favorites, filteredCoins).map(coinKey => 
-            <CoinTile topSection={topSection} coinKey={coinKey}/> 
+            <CoinTile key= {coinKey} topSection={topSection} coinKey={coinKey}/> 
             )}
-        </CoinGridStyled>}
+            </CoinGridStyled>
+        )}
 
     </AppContext.Consumer>
     )}
