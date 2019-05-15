a168ffe0f54e425c8ac75354686f4e045e9f9232
therobertc <rob.jcalderon@gmail.com>
2019-05-15 16:37:44 -0700

    add highcharts theme

---
+++ src/Dashboard/HighchartsConfig.js
@@ -0,0 +1,48 @@
+    export default function() {
+        return {
+        title: {
+        text: 'NETFUND'
+        },
+    
+    
+        yAxis: {
+        title: {
+            text: 'Price'
+        }
+        },
+        legend: {
+        layout: 'vertical',
+        align: 'right',
+        verticalAlign: 'middle'
+        },
+    
+        plotOptions: {
+        series: {
+            label: {
+            connectorAllowed: false
+            },
+            pointStart: 2010
+        }
+        },
+    
+        series: [{
+        name: 'Installation',
+        data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
+        }],
+    
+        responsive: {
+        rules: [{
+            condition: {
+            maxWidth: 500
+            },
+            chartOptions: {
+            legend: {
+                layout: 'horizontal',
+                align: 'center',
+                verticalAlign: 'bottom'
+            }
+            }
+        }]
+        }
+    };
+ }
