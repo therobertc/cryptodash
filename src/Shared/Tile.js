84c2ffb3d50d02e837f4bd48c8f760db996e8ef3
therobertc <rob.jcalderon@gmail.com>
2019-05-14 20:17:44 -0700

    addded coin spotlight

---
+++ src/Shared/Tile.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components'
+import {subtleBoxShadow, lightBlueBackground, greenBoxShadow, redBoxShadow} from '../Shared/Styles'
+
+export const Tile = styled.div`
+${subtleBoxShadow}
+${lightBlueBackground}
+padding: 10px;
+`
+
+export const SelectableTile = styled(Tile)`
+&:hover {
+    cursor: pointer;
+    ${greenBoxShadow}
+
+}`
+
+export const DeletableTile = styled(SelectableTile)`
+&:hover {
+    cursor: pointer;
+    ${redBoxShadow}
+
+}`
+
+export const DisabledTile = styled(Tile)`
+&:hover {
+    pointer-events: none;
+    opacity: 1;
+
+}`
